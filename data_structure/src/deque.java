896073e021e7eb310c987523ac4df3b284f0d623
jdsaeyqo <jdsaeyqi@naver.com>
2021-02-07 12:23:26 +0900

    데크

--- data_structure/src/deque.java
+++ data_structure/src/deque.java
@@ -10,11 +10,13 @@ public static void main(String[] args) {
 	
 	deque.addFirst(1);
 	deque.addFirst(4);
+	deque.offer(3);
 	deque.add(5);
 	deque.add(2);
 	deque.addLast(6);
 	
 	System.out.println(deque);
+	System.out.println(deque.poll());
 	System.out.println(deque.pollFirst());
 	System.out.println(deque.pollLast());
 	
